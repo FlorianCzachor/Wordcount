484b165f57ef9b8702dd3bd5b58a7b39494b331b
florianczachor <czachor@gmx.at>
2022-08-03 11:47:49 +0200

    Iteration 3

--- src/main/java/org/example/Wordcount.java
+++ src/main/java/org/example/Wordcount.java
@@ -2,25 +2,33 @@ package org.example;
 import java.util.*;
 import java.io.*;
 public class Wordcount {
-    public int count(String input) {
+    public int count(String input) throws FileNotFoundException {
         int words = 0;
         List<String> fileContent = new ArrayList<String>();
 
+        File f = new File("src/main/resources/" + input);
         // Reading stopwords.txt file and adding words into ArrayList fileContent
-        try {
-            File f = new File("src/main/resources/stopwords.txt");
+        if (input.contains(".txt") && f.exists() && !f.isDirectory()) {
             Scanner myReader = new Scanner(f);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                fileContent.add(data);
+                String inputWords[] = data.split(" ");
+                for (int i=0; i<inputWords.length; i++) {
+                    fileContent.add(inputWords[i]);
+                }
             }
             myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+            words = fileContent.size();
+        } else {
+            Scanner s = new Scanner(System.in);
+            System.out.print("Please enter a text: ");
+            input = s.nextLine();
+            String inputWords[] = input.split(" ");
+            words = inputWords.length;
         }
 
         // Checking if the input contains any words of stopwords.txt
+        /*
         String inputWords[] = input.split(" ");
         for (int i=0; i<inputWords.length; i++) {
             for (int j=0; j<fileContent.size(); j++) {
@@ -28,14 +36,15 @@ public class Wordcount {
                     words--;
             }
         }
+         */
 
         // Return number of words - number of stopwords
-        return inputWords.length + words;
+        return words;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter text: ");
+        System.out.print("Please enter the filename.txt: ");
         String input = s.nextLine();
         Wordcount w = new Wordcount();
         int i = w.count(input);
