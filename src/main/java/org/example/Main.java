59f6e455152515d4f73a77b00f8a8f64c333a697
Claus Polanka <claus.polanka@chello.at>
2022-08-08 19:13:33 +0200

    Group with New Line

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,15 +6,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         if (args.length == 0) {
-            Scanner s = new Scanner(System.in);
             System.out.print("Enter text: ");
-            String input = s.nextLine();
+            Scanner s = new Scanner(System.in);
+            String userInput = s.nextLine();
+
             Wordcount w = new Wordcount();
-            int count = w.count(input);
+            int count = w.count(userInput);
+
             System.out.println("Number of words: " + count);
         } else {
             Wordcount w = new Wordcount();
             int count = w.count(Paths.get("src/main/resources/" + args[0]));
+
             System.out.println("Number of words: " + count);
         }
     }
